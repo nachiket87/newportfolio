734d2b16aafeb6ed11dc9dec4a6df6096fa4fbee
Nachiket Pusalkar <nachiket87@gmail.com>
2020-11-25 21:56:51 -0500

    why

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,10 @@ import FirstPage from './Firstpage'
 import Project from './Projects'
 import Techstack from './Techstack'
 import Contact from './Contact'
-import SEO from './seo'
 
 const App = () => {
   return (
     <>
-      <SEO />
       <FirstPage />
       <Techstack />
       <Project />
